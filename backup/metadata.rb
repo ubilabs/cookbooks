3c54de15493350e5aa0c09af8acd0172a123927c
Frank <mueller@ubilabs.net>
2011-01-18 17:37:39 +0100

    fix metadata

--- backup/metadata.rb
+++ backup/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Ubilabs GmbH"
 maintainer_email "mueller@ubilabs.net"
 license          "Apache 2.0"
-description      "Installs Backup Gem and Whenever Gem and do all the configuration to send to s3."
+description      "Installs Backup Gem and do all the configuration to send to s3. Automated by cron"
 version          "0.1"
 recipe           "backup::install"
 recipe           "backup::configure"
