5fdd620a15c79b1f783350cf3d49cb3392129a6a
Frank <mueller@ubilabs.net>
2011-01-18 18:08:26 +0100

    cleanup metadata

--- backup/metadata.rb
+++ backup/metadata.rb
@@ -4,6 +4,5 @@ license          "Apache 2.0"
 description      "Installs Backup Gem and do all the configuration to send to s3. Automated by cron"
 version          "0.1"
 recipe           "backup::install"
-recipe           "backup::configure"
 
 supports 'ubuntu'
