ee72ef816985d54a7a10f4d0aa852c20531e1afb
paul koelle <koelle@ubilabs.net>
2011-06-24 17:46:53 +0200

    wrong varnames

--- unicorn/attributes/default.rb
+++ unicorn/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:unicorn] = {}
-default[:unicorn][:config] = "#{application_path}/current/config/unicorn.rb"
+default[:unicorn][:config] = "#{current_release}/config/unicorn.rb"
 default[:unicorn][:base_port] = 4000
 default[:unicorn][:worker_processes] = 4
 
