0885e23677252d8e7417312e5e908f13a6cab1aa
webistrano <webistrano@ubilabs.net>
2011-06-24 12:49:35 +0200

    add unicorn recipe

---
+++ unicorn/attributes/default.rb
@@ -0,0 +1,4 @@
+default[:unicorn] = {}
+default[:unicorn][:base_port] = 4000
+default[:unicorn][:worker_processes] = 4
+
---
+++ unicorn/metadata.rb
@@ -0,0 +1,9 @@
+maintainer "Ubilabs GmbH"
+maintainer_email "koelle@ubilabs.net"
+license "Commercial"
+description "configures unicorn workers"
+version "0.1"
+recipe "unicorn::configure", "Custom unicorn server config"
+
+supports "ubuntu"
+
