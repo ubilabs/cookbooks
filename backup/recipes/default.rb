e9ec65437542907a40a287d02f43820ec4b3cb5b
Frank <frank@heidjer.info>
2011-07-04 09:47:19 +0200

    Rename recipe

--- backup/recipes/default.rb
+++ backup/recipes/default.rb
@@ -3,11 +3,6 @@ gem_package "astrails-safe" do
   action :install
 end
 
-# delete generated config file
-file node[:backup][:config_file] do
-  action :delete
-end
-
 # copy our config file to backup config dir
 template node[:backup][:config_file] do
   source "backup.html.erb"
