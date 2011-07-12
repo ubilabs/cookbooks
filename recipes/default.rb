6367f491d113779c785e4d4b49bb7ab14efd7eff
Frank <frank@heidjer.info>
2011-07-12 11:20:15 +0200

    Add rails deploy recipe

---
+++ recipes/default.rb
@@ -0,0 +1,25 @@
+include_recipe "rails"
+
+node[:deploy].each do |application, deploy|
+
+  if deploy[:application_type] != 'rails'
+    Chef::Log.debug("Skipping deploy::rails application #{application} as it is not an Rails app")
+    next
+  end
+
+  scalarium_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  scalarium_rails do
+    deploy_data deploy
+    app application
+  end
+
+  scalarium_deploy do
+    deploy_data deploy
+    app application
+  end
+end
