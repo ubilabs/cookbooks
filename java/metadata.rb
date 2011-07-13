3cb7b6ea8ec51c2d1985f98cd8ecc86e5eea4a9a
Frank <frank@heidjer.info>
2011-07-13 12:22:00 +0200

    Add simple cookbook to install openjdk-6-jre-headless

---
+++ java/metadata.rb
@@ -0,0 +1,8 @@
+maintainer "Ubilabs GmbH"
+maintainer_email "mueller@ubilabs.net"
+license "Apache 2.0"
+description "Installs openjdk-6-jre-headless"
+version "0.1"
+recipe "java", "Installs openjdk-6-jre-headless"
+
+supports "ubuntu"
---
+++ java/recipes/default.rb
@@ -0,0 +1,3 @@
+package "openjdk-6-jre-headless" do
+  action :install
+end
