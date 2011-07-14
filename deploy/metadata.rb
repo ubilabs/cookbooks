87ecb79e56c1318a18f6ec76a9028dc76b23b2df
Frank <frank@heidjer.info>
2011-07-14 17:58:59 +0200

    Add dependencies to metadata

--- deploy/metadata.rb
+++ deploy/metadata.rb
@@ -3,3 +3,7 @@ maintainer_email "mueller@ubilabs.net"
 description "Deploy applications"
 version "0.1"
 recipe "deploy::custom", "custom deployment for rails"
+
+depends "rails"
+depends "nginx::service"
+depends "unicorn"
