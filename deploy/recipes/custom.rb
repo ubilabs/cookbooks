467db2b73e83a5d9a101ed3a01635bed154b4d54
Frank <frank@heidjer.info>
2011-08-03 09:06:00 +0200

    Temporarily disable asset compression

--- deploy/recipes/custom.rb
+++ deploy/recipes/custom.rb
@@ -69,12 +69,12 @@ node[:deploy].each do |application, deploy|
   nginx_site application
 
 
-  bash "compress the assets" do
-    user "deploy"
-    code <<-EOH
-      cd #{deploy[:current_path]} && bundle exec rake sass:update &&  bundle exec rake jammit:after_deploy
-    EOH
-  end
+  #bash "compress the assets" do
+    #user "deploy"
+    #code <<-EOH
+      #cd #{deploy[:current_path]} && bundle exec rake sass:update &&  bundle exec rake jammit:after_deploy
+    #EOH
+  #end
 
 
   execute "monit reload" do
