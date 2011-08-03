bb9c6c36c9374e2f0b3a07cc0eb1b9634f8b74e7
Frank <frank@heidjer.info>
2011-08-03 16:22:12 +0200

    Also start unicorn service

--- unicorn/recipes/default.rb
+++ unicorn/recipes/default.rb
@@ -16,7 +16,7 @@ node[:deploy].each do |application, deploy|
     service_name "unicorn_#{application}"
 
     supports :start => true, :stop => true, :restart => true, :reload => true
-    action :enable
+    action [:enable, :start]
   end
 
 
