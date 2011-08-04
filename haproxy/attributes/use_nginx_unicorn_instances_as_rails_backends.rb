6cda2a3bb8ae2c9b5cbe0924cb214c003a99d295
Daniel Hüsch <daniel.huesch@gmail.com>
2011-08-04 09:50:03 +0200

    Constrain haproxy adjustment to load balancer instance.

--- haproxy/attributes/use_nginx_unicorn_instances_as_rails_backends.rb
+++ haproxy/attributes/use_nginx_unicorn_instances_as_rails_backends.rb
@@ -1,4 +1,4 @@
-if node[:scalarium][:roles][:nginx_unicorn]
+if node[:scalarium][:instance][:roles].include?('lb') && node[:scalarium][:roles][:nginx_unicorn]
   node[:scalarium][:roles][:nginx_unicorn][:instances].each do |nickname, instance|
     node[:haproxy][:rails_backends] << {'name' => nickname, 'ip' => instance['private_ip'], 'backends' => instance['backends']}
   end
