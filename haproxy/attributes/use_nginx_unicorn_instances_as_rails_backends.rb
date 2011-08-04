c7f8b2d575b04a70a36b1cde0becb8516d0fc04e
Daniel Hüsch <daniel.huesch@gmail.com>
2011-08-04 14:02:05 +0200

    Fix double inclusion of nginx_unicorn instances as rails backends.

--- haproxy/attributes/use_nginx_unicorn_instances_as_rails_backends.rb
+++ haproxy/attributes/use_nginx_unicorn_instances_as_rails_backends.rb
@@ -1,6 +1,7 @@
 if node[:scalarium][:instance][:roles].include?('lb') && node[:scalarium][:roles][:nginx_unicorn]
   node[:scalarium][:roles][:nginx_unicorn][:instances].each do |nickname, instance|
-    node[:haproxy][:rails_backends] << {'name' => nickname, 'ip' => instance['private_ip'], 'backends' => instance['backends']}
+    unless node[:haproxy][:rails_backends].include?(backend = {'name' => nickname, 'ip' => instance['private_ip'], 'backends' => instance['backends']})
+      node[:haproxy][:rails_backends] << backend
+    end
   end
 end
-
