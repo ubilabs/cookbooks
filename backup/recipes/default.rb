d483b8e4fbcb464269739c4a8e5725e852991d3c
Frank <frank@heidjer.info>
2011-08-11 17:07:00 +0200

    Change backup recipe to backup gem

--- backup/recipes/default.rb
+++ backup/recipes/default.rb
@@ -1,5 +1,9 @@
 # install the gems for backup
-gem_package "astrails-safe" do
+gem_package "fog" do
+  action :install
+end
+
+gem_package "backup" do
   action :install
 end
 
@@ -12,8 +16,8 @@ template node[:backup][:config_file] do
 end
 
 # create cron for mysql-backup task
-cron "astrails-safe db and file backup" do
+cron "backup db to s3" do
   hour node[:backup][:cron][:hour].to_s
   minute node[:backup][:cron][:minute].to_s
-  command "astrails-safe #{node[:backup][:config_file]}"
+  command "backup perform --trigger #{node[:backup][:database]} --config_file '#{node[:backup][:config_file]}'"
 end
