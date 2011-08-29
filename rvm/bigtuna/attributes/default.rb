10eb94e18130cbaa58223ed6a1e2b3b591c7e9d0
Klaus Trainer <klaus_trainer@posteo.de>
2011-08-29 11:51:47 +0200

    Remove crap accidently put there.

--- rvm/bigtuna/attributes/default.rb
+++
@@ -1,7 +0,0 @@
-default[:bigtuna][:data_dir] = "/usr/local/var/lib/bigtuna"
-default[:bigtuna][:deploy_dir] = "/usr/local/var/lib/bigtuna/current"
-default[:bigtuna][:run_dir] = "/usr/local/var/run/bigtuna"
-default[:bigtuna][:log_dir] = "/usr/local/var/log/bigtuna"
-default[:bigtuna][:user] = "bigtuna"
-default[:bigtuna][:production_port] = 8081
-default[:bigtuna][:secure_production_port] = 8080
