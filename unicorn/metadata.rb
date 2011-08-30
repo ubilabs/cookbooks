9e82149170a9bc4fd39ca3a83e01a72074261ab9
Frank <frank@heidjer.info>
2011-08-30 15:00:28 +0200

    Change cookbooks to use with new scalarium nginx/unicorn stack

--- unicorn/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer "Ubilabs GmbH"
-maintainer_email "mueller@ubilabs.net"
-license "Apache 2.0"
-description "configures unicorn init scripts"
-version "0.1"
-recipe "unicorn", "Custom unicorn init config"
-
-supports "ubuntu"
-
