d6886166aee592dd2e4aff0b82a120af27573e36
Klaus Trainer <klaus_trainer@posteo.de>
2011-09-01 14:06:22 +0200

    Fix jammit recipe.

--- deploy/recipes/jammit.rb
+++ deploy/recipes/jammit.rb
@@ -2,7 +2,7 @@ node[:deploy].each do |application, deploy|
   bash "compress the assets" do
     user "deploy"
     code <<-EOH
-      cd #{deploy[:current_path]} && bundle exec rake jammit:after_deploy
+      cd #{deploy[:current_path]} && RAILS_ENV=#{deploy[:rails_env]} bundle exec rake jammit:after_deploy
     EOH
   end
 end
