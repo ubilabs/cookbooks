882152a037436457b5c8e98fade842bb97f4bf32
Klaus Trainer <klaus_trainer@posteo.de>
2011-09-01 16:20:06 +0200

    Remove `jammit.rb`: do that in `deploy/before_restart.rb`.

--- deploy/recipes/jammit.rb
+++
@@ -1,9 +0,0 @@
-node[:deploy].each do |application, deploy|
-  bash "compress the assets" do
-    user "deploy"
-    code <<-EOH
-      cd #{deploy[:current_path]} && RAILS_ENV=#{deploy[:rails_env]} bundle exec rake jammit:after_deploy
-    EOH
-  end
-end
-
