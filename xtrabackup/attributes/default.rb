b2aff585a668f296557374e2b689d4a811e2146d
Frank <frank@heidjer.info>
2011-09-01 16:24:28 +0200

    Remove xtrabackup cookbook

--- xtrabackup/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default[:xtrabackup][:version] = '1.6'
-default[:xtrabackup][:target_dir] = '/vol/backups/mysql'
-default[:xtrabackup][:executable] = "/opt/local/bin/db-backup"
-default[:xtrabackup][:keep] = 30
-default[:xtrabackup][:schedule] = ''
-default[:xtrabackup][:deb] = 'xtrabackup_1.6.2-274.lucid_i386.deb'
