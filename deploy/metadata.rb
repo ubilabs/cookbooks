3276a0eefce6e626b5891eba63e5c9dd221ae518
Klaus Trainer <klaus_trainer@posteo.de>
2011-09-01 16:21:14 +0200

    Remove obsolete deploy cookbook.

--- deploy/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer "Ubilabs GmbH"
-maintainer_email "mueller@ubilabs.net"
-description "Deploy applications"
-version "0.1"
-recipe "deploy::custom", "custom deployment for rails"
-
-depends "rails"
-depends "nginx::service"
-depends "unicorn"
