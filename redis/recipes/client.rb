202015c2ef5b3c731f4b7b46d69a70f3f454172e
Klaus Trainer <klaus_trainer@posteo.de>
2011-12-20 11:55:24 +0100

    Put `redis.yml` into `shared/config`.

--- redis/recipes/client.rb
+++ redis/recipes/client.rb
@@ -10,14 +10,12 @@ node[:deploy].each do |application, deploy|
     end
   redis_host = "#{redis_server_ip}:#{node[:redis][:port]}"
 
-  template "#{deploy[:deploy_to]}/current/config/redis.yml" do
+  template "#{deploy[:deploy_to]}/shared/config/redis.yml" do
     source "redis.yml.erb"
     mode "0660"
     owner deploy[:user]
     group deploy[:group]
     variables(:host => redis_host)
-    only_if do
-      File.directory?("#{deploy[:deploy_to]}/current")
-    end
   end
+
 end
