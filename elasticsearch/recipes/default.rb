d269ad29fb9d01187a4dd49e32f0250504393641
Frank <frank@heidjer.info>
2012-03-23 15:50:25 +0100

    Revert "Always install elasticsearch monit config"
    This reverts commit 380c20755e656e9bf69cd28576feb3139a87526e.

--- elasticsearch/recipes/default.rb
+++ elasticsearch/recipes/default.rb
@@ -140,4 +140,5 @@ end
 
 # Add Monit configuration file
 #
-monitrc("elasticsearch", :pidfile => "#{node.elasticsearch[:pid_path]}/#{node.elasticsearch[:node_name].to_s.gsub(/\W/, '_')}.pid")
+monitrc("elasticsearch", :pidfile => "#{node.elasticsearch[:pid_path]}/#{node.elasticsearch[:node_name].to_s.gsub(/\W/, '_')}.pid") \
+  if node.recipes.include?('monit')
