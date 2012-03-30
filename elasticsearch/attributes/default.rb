484c6ad246f4e0ff2b2e282bad32e4af5bda6847
Frank <frank@heidjer.info>
2012-03-30 09:17:38 +0200

    Decrease max_mem

--- elasticsearch/attributes/default.rb
+++ elasticsearch/attributes/default.rb
@@ -20,7 +20,8 @@ default.elasticsearch[:pid_path]  = "/usr/local/var/run/elasticsearch"
 # Maximum amount of memory to use is automatically computed as 2/3 of total available memory.
 # You may choose to configure it in your node configuration instead.
 #
-max_mem = "#{(node.memory.total.to_i - (node.memory.total.to_i/3) ) / 1024}m"
+#max_mem = "#{(node.memory.total.to_i - (node.memory.total.to_i/3) ) / 1024}m"
+max_mem = "#{(node.memory.total.to_i - (node.memory.total.to_i/2) ) / 1024}m"
 default.elasticsearch[:min_mem] = "128m"
 default.elasticsearch[:max_mem] = max_mem
 
