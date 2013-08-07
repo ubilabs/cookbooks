b6dbf4c5023e3e6238eb73d14ab1d0cd93d0965b
Frank <frank@heidjer.info>
2013-08-07 09:45:11 +0200

    Not sure if necessary.

--- nodejs/metadata.rb
+++ nodejs/metadata.rb
@@ -6,5 +6,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.4.1"
 recipe           "nodejs", "Installs Node.JS from source"
 recipe           "nodejs::npm", "Installs npm - a package manager for node"
+recipe           "nodejs::package", "Installs nodejs and npm from package manager"
 
 depends          "build-essential"
